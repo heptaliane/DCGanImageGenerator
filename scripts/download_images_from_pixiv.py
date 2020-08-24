aa49f79e6b86b27491a6f7ab33ba28dbc7485929
heptaliane <heptaliane@gmail.com>
2020-08-24 18:21:37 +0900

    enable to download image by user id

--- scripts/download_images_from_pixiv.py
+++ scripts/download_images_from_pixiv.py
@@ -22,12 +22,25 @@ def parse_arguments(argv):
                         help='Path to output base directory')
     parser.add_argument('--save_as', default=None,
                         help='Save images as a given label')
-    parser.add_argument('--keyword', '--key', '-k', required=True,
+    parser.add_argument('--keyword', '--key', '-k', default=None,
                         help='Search image keyword')
+    parser.add_argument('--user_id', '--user', '-u', type=int, default=None,
+                        help='Search by given user id')
     parser.add_argument('--auth_json', '--auth', '-a',
                         default='.auth.json',
                         help='Path to pixiv authenticatation file')
+    parser.add_argument('--allow_r18', action='store_true',
+                        help='Allow R18 images')
+    parser.add_argument('--allow_manga', action='store_true',
+                        help='Allow manga images')
+    parser.add_argument('--threshold_favorite', '-t', type=int, default=1000,
+                        help='Threshold of the number of favorite')
     args = parser.parse_args()
+
+    if args.keyword is None and args.user_id is None:
+        logger.error('"--keyword" or "--user" argument is required.')
+        args.print_help()
+        exit()
 
     return args
 
@@ -35,11 +48,15 @@ def parse_arguments(argv):
 class PixivDownloader():
     _NAME_FORMAT = 'pixiv_%s_%03d.jpg'
 
-    def __init__(self, dst_dir):
+    def __init__(self, dst_dir, th_favorite=1000,
+                 allow_r18=False, allow_manga=False):
         self._app = pixivpy3.AppPixivAPI()
         self._api = pixivpy3.PixivAPI()
         self._is_auth = False
         self._dst_dir = dst_dir
+        self.th_favorite = th_favorite
+        self.allow_manga = allow_manga
+        self.allow_r18 = allow_r18
         os.makedirs(dst_dir, exist_ok=True)
 
     def authenticate(self, username, password):
@@ -66,18 +83,33 @@ class PixivDownloader():
                                    name=self._NAME_FORMAT % (illust_id, i),
                                    path=self._dst_dir)
 
-    def __call__(self, word):
+    def _filter_illusts(self, res, user_id):
+        if res.stats.scored_count < self.th_favorite:
+            return False
+        if not self.allow_r18 and res.sanity_level != 'white':
+            return False
+        if not self.allow_manga and res.type == 'manga':
+            return False
+        return user_id is None or res.user_id != user_id
+
+    def __call__(self, keyword=None, user_id=None):
         if not self._is_auth:
             logger.info('Not authenticated.')
             return
 
         for i in range(1, 1000):
-            data = self._api.search_works(word, mode='tag', page=i)
+            if keyword is not None:
+                data = self._api.search_works(word, mode='tag', page=i)
+            elif user_id is not None:
+                data = self._api.users_works(user_id, page=i)
+
             if data.status != 'success':
                 break
+            if i == 1:
+                print(data.response[0].keys())
 
             for res in data.response:
-                if res.stats.scored_count > 1000:
+                if self._filter_illusts(res, user_id):
                     self._download_images(res.id)
 
 
@@ -85,10 +117,13 @@ def main(argv):
     args = parse_arguments(argv)
 
     if args.save_as is None:
-        out_dir = os.path.join(args.out_dir, base64_encode(args.keyword))
+        label = str(args.user_id) if args.keyword is None \
+            else base64_encode(args.keyword)
+        out_dir = os.path.join(args.out_dir, label)
     else:
         out_dir = os.path.join(args.out_dir, base64_encode(args.save_as))
-    downloader = PixivDownloader(out_dir)
+    downloader = PixivDownloader(out_dir, args.threshold_favorite,
+                                 args.allow_r18, args.allow_manga)
 
     auth = read_json(args.auth_json)
     auth = dict() if auth is None else auth
@@ -107,7 +142,7 @@ def main(argv):
             logger.error('Failed to login.')
             del auth['pixiv']
 
-    downloader(args.keyword)
+    downloader(keyword=args.keyword, user_id=args.user_id)
 
 
 if __name__ == '__main__':
